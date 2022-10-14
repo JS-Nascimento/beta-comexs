20dcc06a7da00c821b5ef9ea38e009da6b36893d
JorgeInfnet <99371664+JorgeInfnet@users.noreply.github.com>
2022-10-14 00:22:17 -0300

    Componente Product Card

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
-  <v-app >
+  <v-app>
     <v-layout>
       <v-main>
         <NavComexs></NavComexs>
+        <v-container>
+          <v-row class="mx-auto justify-center">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </v-row>
+        </v-container>
       </v-main>
     </v-layout>
   </v-app>
@@ -10,12 +36,12 @@
 
 <script>
 import NavComexs from './components/NavComexs.vue'
-
+import ProductCard from './components/ProductCard.vue'
 export default {
   name: 'App',
 
   components: {
-    NavComexs
+    NavComexs, ProductCard
   },
 
   data: () => ({
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-card
+    class="h-full d-flex flex-column mx-3 my-3"
+    max-width="265" 
+  >
+    <v-img
+      src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+      height="150px"
+      max-height="150px"
+      cover
+    ></v-img>
+
+    <v-card-title>
+      Lapis de Cor 12 cores Faber Castell
+    </v-card-title>
+
+    <v-card-subtitle>
+      1,000 miles of wonder
+    </v-card-subtitle>
+
+    <v-card-actions>
+      <v-btn
+        color="orange-lighten-2"
+        variant="text"
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+        @click="show = !show"
+      ></v-btn>
+    </v-card-actions>
+
+    <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+  </v-card>
+</template>
+
+<script>
+export default {
+    name:'ProductCard',
+    data: () => ({
+      show: false,
+    }),
+}
+</script>
+
+<style>
+
+</style>
